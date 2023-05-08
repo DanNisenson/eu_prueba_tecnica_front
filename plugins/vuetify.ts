6ea806552926b3d41f11c1375134abbd730e95c7
DanNisenson <danpablo1@gmail.com>
2023-05-08 18:59:44 +0200

    ✨Finish vuetify setup and add theme

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,12 +1,17 @@
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { md3 } from "vuetify/blueprints";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     ssr: true,
     components,
     directives,
+    blueprint: md3,
+    theme: {
+      defaultTheme: "dark",
+    },
   });
 
   nuxtApp.vueApp.use(vuetify);
