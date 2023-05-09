d47b0b3b3374a33f91cfbee7b9c831428f77c747
DanNisenson <danpablo1@gmail.com>
2023-05-09 15:42:10 +0200

    👷Add checkbox icons components

---
+++ components/icons/CheckboxCircleDone.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg-icon class="icon" height="1rem" type="mdi" :path="path"></svg-icon>
+</template>
+
+<script>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiCheckboxMarkedCircleOutline } from "@mdi/js";
+
+export default {
+  components: {
+    SvgIcon,
+  },
+  data() {
+    return {
+      path: mdiCheckboxMarkedCircleOutline,
+    };
+  },
+};
+</script>
+
+<style>
+.icon {
+  cursor: pointer;
+}
+</style>
