597bb2f3d3c44d5650715309cff34a0a99ebcebd
DanNisenson <danpablo1@gmail.com>
2023-05-09 17:43:05 +0200

    🗄Merge circle checkbox components

---
+++ components/icons/CheckboxCircle.vue
@@ -0,0 +1,44 @@
+<template>
+  <svg-icon
+    v-if="checked"
+    class="icon"
+    height="1rem"
+    type="mdi"
+    :path="pathDone"
+  ></svg-icon>
+  <svg-icon
+    v-else
+    class="icon"
+    height="1rem"
+    type="mdi"
+    :path="pathBlank"
+  ></svg-icon>
+</template>
+
+<script>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiCheckboxMarkedCircleOutline } from "@mdi/js";
+import { mdiCheckboxBlankCircleOutline } from "@mdi/js";
+
+export default defineComponent({
+  components: {
+    SvgIcon,
+  },
+  props: {
+    checked: { type: Boolean, default: false },
+  },
+  data() {
+    return {
+      pathDone: mdiCheckboxMarkedCircleOutline,
+      pathBlank: mdiCheckboxBlankCircleOutline,
+    };
+  },
+});
+</script>
+
+<style>
+.icon {
+  cursor: pointer;
+  margin: 0 6px;
+}
+</style>
