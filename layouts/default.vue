1d12bc317ab2cb6a0895c4713f9745c2869ec2f5
DanNisenson <danpablo1@gmail.com>
2023-05-10 23:41:26 +0200

    ✨Add favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer model-value class="pt-4" color="grey-darken-4" rail>
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
-        :size="n === 1 ? 36 : 20"
-        class="d-block text-center mx-auto mb-9"
-      ></v-avatar>
+      <v-avatar size="36px" class="d-block text-center mx-auto mb-9">
+        <v-avatar
+          v-for="n in 6"
+          :key="n"
+          :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
+          :size="n === 1 ? 36 : 20"
+          class="d-block text-center mx-auto mb-9"
+        ></v-avatar>
+      </v-avatar>
     </v-navigation-drawer>
 
     <v-main>
